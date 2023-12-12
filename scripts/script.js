eafda8c94a17e62d2e6cdb453ea1a896696c7d55
urostodorovic95 <150077866+urostodorovic95@users.noreply.github.com>
2023-12-12 22:41:12 +0100

    Create new divs with a user-defined size

--- scripts/script.js
+++ scripts/script.js
@@ -5,4 +5,12 @@ const gridContainer = document.querySelector("#container-grid");
 function createGrid(gridSize) {
     // for gridSize integer create a grid with:
     // horizontal - vertical same size
+    for (let i=0; i < gridSize*gridSize; i++) {
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("new-div");
+        gridContainer.appendChild(newDiv);
+    }
 }
+
+// debug:
+console.log(createGrid(4))
