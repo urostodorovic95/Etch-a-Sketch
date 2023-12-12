6c0e75d1fe29aa9f4747b2b2d668535d59598ed7
urostodorovic95 <150077866+urostodorovic95@users.noreply.github.com>
2023-12-12 22:30:39 +0100

    Add basic HTML, start to plan JS

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+// document elements
+const gridContainer = document.querySelector("#container-grid");
+
+// create a grid
+function createGrid(gridSize) {
+    // for gridSize integer create a grid with:
+    // horizontal - vertical same size
+}
