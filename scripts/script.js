fcf125f099ce7a9b68910d6810b2885122930d16
urostodorovic95 <150077866+urostodorovic95@users.noreply.github.com>
2023-12-12 23:05:54 +0100

    Distribute and display the boxes correctly

--- scripts/script.js
+++ scripts/script.js
@@ -3,14 +3,18 @@ const gridContainer = document.querySelector("#container-grid");
 
 // create a grid
 function createGrid(gridSize) {
-    // for gridSize integer create a grid with:
-    // horizontal - vertical same size
+    const boxSize = `${100 / gridSize}%`;
     for (let i=0; i < gridSize*gridSize; i++) {
         let newDiv = document.createElement("div");
         newDiv.classList.add("new-div");
+        newDiv.style.width = boxSize;
+        newDiv.style.height = boxSize;
+        newDiv.style.boxSizing = "border-box";
         gridContainer.appendChild(newDiv);
     }
 }
 
+
+
 // debug:
-console.log(createGrid(4))
+console.log(createGrid(20))
