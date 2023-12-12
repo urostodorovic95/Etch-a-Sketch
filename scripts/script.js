e1de958135f1a137291226b1cb0ea731694c7841
urostodorovic95 <150077866+urostodorovic95@users.noreply.github.com>
2023-12-12 23:41:14 +0100

    Add ability to listen for mouseover & clicked

--- scripts/script.js
+++ scripts/script.js
@@ -3,18 +3,23 @@ const gridContainer = document.querySelector("#container-grid");
 
 // create a grid
 function createGrid(gridSize) {
-    const boxSize = `${100 / gridSize}%`;
-    for (let i=0; i < gridSize*gridSize; i++) {
-        let newDiv = document.createElement("div");
-        newDiv.classList.add("new-div");
-        newDiv.style.width = boxSize;
-        newDiv.style.height = boxSize;
-        newDiv.style.boxSizing = "border-box";
-        gridContainer.appendChild(newDiv);
-    }
+  const boxSize = `${100 / gridSize}%`;
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("new-div");
+    newDiv.style.width = boxSize;
+    newDiv.style.height = boxSize;
+    newDiv.style.boxSizing = "border-box";
+    gridContainer.appendChild(newDiv);
+  }
 }
 
-
+// listen for mouse hovers, make use of bubling
+gridContainer.addEventListener("mouseover", (e) => {
+  if (e.buttons === 1 && e.target.classList.contains("new-div")) {
+    e.target.style.backgroundColor = "white";
+  }
+});
 
 // debug:
-console.log(createGrid(20))
+console.log(createGrid(30));
